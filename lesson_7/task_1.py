87994e9ccc3fa5dc7ea0a1923692c55d93f232b1
Mikhail Tarabrin <51959562+machukhinktato@users.noreply.github.com>
2020-02-13 10:34:02 +0300

    Update task_1.py

--- lesson_7/task_1.py
+++ lesson_7/task_1.py
@@ -14,7 +14,7 @@ def task_1_sort(array):
     while srtd:
         srtd = False
         for i in range(len(array) - 1):
-            if array[i] > array[i + 1]:
+            if array[i] < array[i + 1]:
                 array[i], array[i + 1] = array[i + 1], array[i]
                 srtd = True
 
